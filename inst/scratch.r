ee7c34a626be16c37cc6fa13b46529317811f60b
JinxuanBian <jinxuan.bian@yale.edu>
2022-11-22 23:33:26 -0500

    fix bugs

--- inst/scratch.r
+++ inst/scratch.r
@@ -10,8 +10,8 @@ data(ukb_accel)
 
 dt <- first(ukb_accel$time)
 
-ukb_accel %>%
-  filter(time >= dt  & time < dt + minutes(5)) %>%
+ukb_accel |>
+  filter(time >= dt  & time < dt + minutes(5)) |>
 #  (\(x) x[seq(1, nrow(x), by =100),])() |>
   accel_plot() +
     theme_minimal() +
@@ -19,7 +19,7 @@ ukb_accel %>%
     ylab("Acceleration (in gravities)")
 
 ukb_accel %>%
-  filter(time >= dt & time < dt + minutes(5)) %>%
-  spectral_signature(take_log = FALSE) %>%
+  filter(time >= dt & time < dt + minutes(5)) |>
+  spectral_signature(take_log = FALSE) |>
  # filter(freq <= 10) |>
   accel_plot()
